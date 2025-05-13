783f1e21917e643eb0b0c2de53501d3bb9b08fde
Niflan <niflanseven@gmail.com>
2025-05-13 23:13:42 +0530

    Project init to gitHub repository

---
+++ src/main/java/lk/ijse/cmjd108/LostandFound2025/LostandFound2025Application.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd108.LostandFound2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostandFound2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostandFound2025Application.class, args);
+	}
+
+}
